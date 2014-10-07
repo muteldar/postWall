055b73bb2409a4c02a6cc12dd5e9ca9a68bf1706
mat <mat@n00bworks.com>
2014-10-07 19:43:24 -0400

    removing uneeded files/using cdn for some of these.

--- admin/index.php
+++ admin/index.php
@@ -29,8 +29,7 @@ if(Login_Check())
 			<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 			<link rel="stylesheet" type="text/css" href="css/datepicker.css">
 			<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-			<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-			<script type="text/javascript" src="js/bootstrap-datepicker.js"></script>
+			<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js"></script>
 			<script>
 					$(function(){
 								$("#datepicker").datepicker({format: "mm-dd-yyyy"});
